9070d13efc047b05ee887f2ed4f2a4476074393a
Yash Raj Singh <98258627+yrs147@users.noreply.github.com>
2023-01-04 17:38:18 +0530

    Update Hero.js

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
-          Musician App
+          Musician App v1
         </div>
       </div>
     </section>
